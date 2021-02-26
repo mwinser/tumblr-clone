39b27a31cd5d9093a47d643bb305a559d3dc4843
Michael Winser <mike.winser@gmail.com>
2021-02-26 11:59:02 -0800

    code cleanup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,19 +5,33 @@ function Login() {
         <div className="container-2xl w-screen h-screen relative bg-login bg-cover flex flex-col justify-center items-center">
             <nav className="fixed top-0 left-0 h-8 flex justify-between items-center w-full bg-transparent mt-2 px-2">
                 <div className="flex">
-                    <p className="font-bold text-4xl p-1 text-white">t</p>
+                    <p className="font-bold text-4xl p-1 text-white">
+                        t
+                    </p>
                     <input type="text" placeholder="Search" className="bg-transparent bg-white bg-opacity-25 rounded placeholder-gray-100 p-1 m-2"/>
                 </div>
-                <button className="bg-green-300 rounded p-2 font-bold">Sign up</button>
+                <button className="bg-green-300 rounded p-2 font-bold">
+                    Sign up
+                </button>
             </nav>
             <div className="max-w-xs  p-8 flex flex-col justify-center align-center">
-                <p className="font-bold text-7xl text-center text-white mb-2" >tumblr</p>
-                <p className="font-bold text-lg text-center text-white mt-2">Make stuff, look at stuff, talk about stuff, find your people.</p>
-                <input type="email" placeholder="Email" className="text-md mt-2 p-2 rounded"></input>
-                <button className="bg-green-300 rounded p-2 font-bold mt-2 mb-8">Next</button>
+                <p className="font-bold text-7xl text-center text-white mb-2" >
+                    tumblr
+                </p>
+                <p className="font-bold text-lg text-center text-white mt-2">
+                    Make stuff, look at stuff, talk about stuff, find your people.
+                </p>
+                <input type="email" placeholder="Email" className="text-md mt-2 p-2 rounded"/>
+                <button className="bg-green-300 rounded p-2 font-bold mt-2 mb-8">
+                    Next
+                </button>
                 <div className="flex justify-around items-center mt-8">
-                    <button className="bg-black rounded p-2 text-sm font-bold text-white border border-white">App Store</button>
-                    <button className="bg-black rounded p-2 text-sm font-bold text-white border border-white">Google Play</button>
+                    <button className="bg-black rounded p-2 text-sm font-bold text-white border border-white">
+                        App Store
+                    </button>
+                    <button className="bg-black rounded p-2 text-sm font-bold text-white border border-white">
+                        Google Play
+                    </button>
                 </div>
             </div>
             <footer className="fixed bottom-0 left-0 h-8 flex justify-between items-center w-full bg-transparent mb-2 px-2">
