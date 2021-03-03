0e01626ba29008514c38e93275ead4d4509bf55e
Michael Winser <mike.winser@gmail.com>
2021-03-03 08:14:52 -0800

    added CreatePost page stub

---
+++ src/pages/CreatePost.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function CreatePost() {
+ 
+        return (
+        <>
+            <Header/>
+            <div className="bg-navy h-screen max-w-990px mx-auto mt-10 px-2 flex items-start text-white">
+                Create Post
+            
+            <div></div>
+
+
+            </div>
+            
+        </>
+    )
+}
+
+export default CreatePost
