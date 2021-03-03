f66d36d24fbf0478c611da87539b61d8e0864693
Michael Winser <mike.winser@gmail.com>
2021-03-03 08:15:18 -0800

    added CreatePost routing

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const Search = lazy(()=> import('./pages/Search'))
 const Inbox = lazy(()=> import('./pages/Inbox'))
 const Blog = lazy(()=> import('./pages/Blog'))
 const Recommended = lazy(()=> import('./pages/Recommended'))
+const CreatePost = lazy(()=> import('./pages/CreatePost'))
 
 
 function App() {
@@ -20,15 +21,17 @@ function App() {
     <div className="relative w-full h-full bg-navy">
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <PrivateRoute exact path={ROUTES.DASHBOARD} component={Dashboard}/>
+          <PrivateRoute exact path="/" component={Dashboard}/>
           <Route path={ROUTES.LOGIN} component={Login}/>
           <Route path={ROUTES.SIGN_UP} component={Signup}/>
           <Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword}/>
+          <PrivateRoute path={ROUTES.DASHBOARD} component={Dashboard}/>
           <PrivateRoute path={ROUTES.PROFILE} component={Profile}/>
           <PrivateRoute path={ROUTES.SEARCH} component={Search}/>
           <PrivateRoute path={ROUTES.INBOX} component={Inbox}/>
           <PrivateRoute path={ROUTES.BLOG} component={Blog}/>
           <PrivateRoute path={ROUTES.RECOMMENDED} component={Recommended}/>
+          <PrivateRoute path={ROUTES.CREATE_POST} component={CreatePost}/>
           <PrivateRoute component={NotFound}/>
         </Switch>
       </Suspense>
