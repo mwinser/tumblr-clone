283c8d4486a14eed25acd18079b1ec93c011bcbf
Michael Winser <mike.winser@gmail.com>
2021-03-06 16:22:39 -0800

    cursor-pointers

--- src/components/Post.js
+++ src/components/Post.js
@@ -67,7 +67,7 @@ function Post(props){
                 )
             }
             <span className="flex-grow"></span>
-            <div className="MENU">...</div>
+            <div className="MENU font-bold text-xl">...</div>
         </div>
         {item.type==='image' && (
             <div className="IMAGE">
@@ -108,7 +108,7 @@ function Post(props){
                     <svg viewBox="0 0 17 18.1" width="21" height="21" fill="rgba(var(--black), 0.65)"><path d="M12.8.2c-.4-.4-.8-.2-.8.4v2H2c-2 0-2 2-2 2v5s0 1 1 1 1-1 1-1v-4c0-1 .5-1 1-1h9v2c0 .6.3.7.8.4L17 3.6 12.8.2zM4.2 17.9c.5.4.8.2.8-.3v-2h10c2 0 2-2 2-2v-5s0-1-1-1-1 1-1 1v4c0 1-.5 1-1 1H5v-2c0-.6-.3-.7-.8-.4L0 14.6l4.2 3.3z"></path></svg>
                 </div>
                 <div 
-                    className="FAVORITE ml-5 fill-current" 
+                    className="FAVORITE ml-5 fill-current cursor-pointer" 
                     onClick={()=>handleToggleFavorite()}
                 >
                 {isUserFavorite? (
