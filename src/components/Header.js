b5d0d4c6db71ab7657e7dd9150cf1baba6b9484f
Mike Winser <mike.winser@14four.com>
2022-01-30 09:46:55 -0800

    fixed header width on small screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,9 +31,9 @@ function Header(){
             {isCreateMenuOpen && <CreatePost/>}
             
             {/* >1024px vw header */}
-            <div className="relative max-w-screen-2xl w-full flex items-center justify-between h-14 text-white  mx-auto, pl-6 pr-5">
+            <div className="relative max-w-screen-2xl w-full flex items-center justify-between h-14 text-white  mx-auto sm:pl-6 sm:pr-5">
                 
-                <div className="flex m-2 p-1 justify-center items-center">
+                <div className="flex my-2 mx-4 justify-center items-center">
                     <Link to={ROUTES.DASHBOARD}>
                         <svg viewBox="0 0 21 36.8" width="20" height="33" fill="white">
                             <path d="M21 36.75h-6.2c-5.577 0-9.733-2.844-9.733-9.646V16.21H0v-5.9C5.576 8.876 7.909 4.12 8.177 0h5.79v9.354h6.757v6.856h-6.756v9.486c0 2.843 1.448 3.826 3.753 3.826h3.271L21 36.75z"></path>
